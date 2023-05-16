09d546973a2933df8257d1b308902fdadfcf4266
Aijaz Ahmed <aijaz.ahmed.bz@gmail.com>
2023-05-16 08:01:56 +0500

    updated on 16-May-23

---
+++ react-class/app/components/Avatar.tsx
@@ -0,0 +1,6 @@
+export default function Avater() {
+  const avatar = "https://i.imgur.com/MK3eW3Am.jpg";
+  const discription = "Katherine Johnson";
+
+  return <img className="avater" src={avatar} alt={discription} />;
+}
--- react-class/app/page.tsx
+++ react-class/app/page.tsx
@@ -1,11 +1,13 @@
+import Avater from "./components/Avatar";
 import Gallery, { NamedProfile } from "./components/Gallery";
 import Profile from "./components/Profile";
 import Todo from "./components/ToDo";
+import TodoList from "./components/ToDoList";
 
 export default function Home() {
   return (
     <>
-      <Todo />
+      <TodoList />
     </>
   );
 }
